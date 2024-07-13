a7d4a8f4faf09db1626ea56074a18e36b5dd6f36
drondeseries <108965325+drondeseries@users.noreply.github.com>
2024-07-13 15:03:11 -0400

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change directory to where main.py is located
-cd /path/t/plexPreCacherclonenextEpisode/
+cd /path/to/plexPreCacherclonenextEpisode/
 
 # Run the Python script
 python3 main.py
